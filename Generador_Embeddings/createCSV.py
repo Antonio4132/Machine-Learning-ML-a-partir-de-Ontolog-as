914a47dacb2c8852111d46bf0e584019208d6822
Antonio4132 <64072456+Antonio4132@users.noreply.github.com>
2021-09-19 10:46:22 +0200

    Add files via upload

---
+++ Generador_Embeddings/createCSV.py
@@ -0,0 +1,22 @@
+import gensim
+from gensim.models import KeyedVectors
+import pandas as pd
+
+
+kv = KeyedVectors.load('embkv')
+
+projects = []
+
+for idx, key in enumerate(kv.vocab):
+    if 'PROJECT_ID_' in key:
+        projects.append(key)
+        
+sort = sorted(projects)
+
+
+ordered_vocab = [(term.replace('<http://www.semanticweb.org/alicia/ontologies/2020/8/singleCellRepositories#','').replace('>',''), voc.index, voc.count) for term, voc in kv.vocab.items() if 'PROJECT_ID_' in term]
+ordered_vocab = sorted(ordered_vocab, key=lambda k: k[0])
+ordered_terms, term_indices, term_counts = zip(*ordered_vocab)
+word_vectors = pd.DataFrame(kv.syn0[term_indices, :], index=ordered_terms)
+
+word_vectors.to_csv('projects_kv.csv')
